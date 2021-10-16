d4415c9cdde6eca04e4bba5ad1442a41ecfb8792
Muammar Johnson <muammarj@gmail.com>
2021-10-16 19:49:30 -0400

    days end tests failing, need to map customer to correct spots

--- appointments/src/AppointmentDetailView.js
+++ appointments/src/AppointmentDetailView.js
@@ -2,9 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-export const  AppointmentDetailView = ()=>{
+export const  AppointmentDetailView = ({customer})=>{
 return(
     <div id="appointmentDetailView">
+        <table id='appointmentDetailViewTable'>
+            <tr>
+                <th>Item</th>
+                <th>Details</th>
+            </tr>
+            <tr>
+                <td>
+                Customer
+                </td>
+                <td id='customer'>
+                    {`${customer.firstName} ${customer.lastName}`}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                Phone Number
+                </td>
+                <td id='phone_number'>
+                    {customer.phoneNumber}
+                </td>
+            </tr>
+            <tr>
+                <td>Stylist</td>
+                <td id='stylist'>{customer.stylist}</td>
+            </tr>
+            <tr>
+                <td>Service</td>
+                <td id='service'>{customer.service}</td>
+            </tr>
+            <tr>
+                <td>notes</td>
+                <td id='notes'> {customer.notes}</td>
+            </tr>
+        </table>
         
     </div>
 )
--- appointments/test/AppointmentDetailView.test.js
+++ appointments/test/AppointmentDetailView.test.js
@@ -5,6 +5,8 @@ import {AppointmentDetailView} from "../src/AppointmentDetailView";
 
 describe('AppointmentDetailView', () => {
     let container;
+    const dummyCustomer =  {firstName: 'Charlie', lastName: "Nash", phoneNumber:'(554) 338-2121', stylist:"Maggie", service:"Beard Trim", notes:"yo no necesito el notes para yo tengo la tareia"};
+
     beforeEach(()=>{
         container = document.createElement('div');
     })
@@ -14,5 +16,22 @@ describe('AppointmentDetailView', () => {
         expect(container.querySelector('div#appointmentDetailView')).not.toBeNull();
     }
     )
+    it("make sure table exists, has 2 columns and 5 rows",()=>{
+        const tableQuery = 'table#appointmentDetailViewTable';
+        ReactDOM.render(<AppointmentDetailView />, container);
+        expect(container.querySelector(tableQuery)).not.toBeNull();
+        expect(container.querySelectorAll('th')).toHaveLength(2);
+        expect(container.querySelectorAll('tr')).toHaveLength(6);
+    })
+    it("make sure customer data is filling up appropriate place on table ", ()=>{
+            
+        console.log(dummyCustomer.firstName);
+        ReactDOM.render(<AppointmentDetailView customer={dummyCustomer}/>, container);
+        expect(container.querySelector("td#customer")).toBe(`${dummyCustomer.firstName} ${dummyCustomer.lastName}`);
+        expect(container.querySelector("td#phone_number")).toBe(dummyCustomer.phoneNumber);
+        expect(container.querySelector("td#stylist")).toBe(dummyCustomer.stylist);
+        expect(container.querySelector('td#service')).toBe(dummyCustomer.service);
+        expect(container.querySelector('td#notes')).toBe(dummyCustomer.notes);
+    })
 
 })
